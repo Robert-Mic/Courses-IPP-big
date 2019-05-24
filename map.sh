41ea07939ccd1053cd9c28ab4d0a60f9ae74398b
Rhanotlq <robertos991@wp.pl>
2019-05-24 17:11:33 +0200

    map.sh change

--- map.sh
+++ map.sh
@@ -8,21 +8,34 @@ file=$1
 shift
 routes=$@
 re='^[0-9]+$'
+exitcode=1
 
 for route in $routes; do
 	if ! [[ $route =~ $re ]] ; then
 		exit 1
-	else
-		xd=$(grep "^$route;" $file)
-		numbers=$(echo $xd | awk -F ';' '{for (i=3;i<=NF;i+=3) print $i}')
-		sum=0
-		for num in $numbers; do
-			sum=$(($sum + $num))
-		done
-		if [ $sum -ne 0 ]; then
-			echo $sum
-		fi
+	fi
+	if [[ $route -lt 1 ]]; then
+		exit 1
+	fi
+	if [[ 999 -lt $route ]]; then
+		exit 1
+	fi
+	
+	exitcode=0
+	xd=$(grep "^$route;" $file)
+	numbers=$(echo $xd | awk -F ';' '{for (i=3;i<=NF;i+=3) print $i}')
+	sum=0
+	
+	for num in $numbers; do
+		sum=$(($sum + $num))
+	done
+	if [ $sum -ne 0 ]; then
+		echo "$route;$sum"
 	fi
 done
 
-exit 0
+if [ $exitcode -ne 0 ]; then
+	exit 1
+else
+	exit 0
+fi
